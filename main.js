5e62a1b274f4d4e6dcca625e4ebfb4df3d496eca
Tiapina Arina <tiapinaarina@gmail.com>
2023-05-15 19:21:08 +0300

    Burger, footer
    Добавлена надпись к бургеру для логичности иконки, изменена композиция футтера для мобильной версии

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const sandwichmenu = document.querySelector('.menu-burger');
 const menu = document.querySelector('.site-navigation');
 
-sandwichmenu.addEventListener('click', function(e) {
+sandwichmenu.addEventListener('click', function (e) {
   e.preventDefault();
   menu.classList.toggle('menu-active');
 });
